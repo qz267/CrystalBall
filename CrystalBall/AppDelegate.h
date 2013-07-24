c944c61d9d99c932d9a24ab8e35818ecef60b5fc
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-23 23:54:33 -0400

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Zheng Qin on 7/23/13.
+//  Copyright (c) 2013 Zheng Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Zheng Qin on 7/23/13.
+//  Copyright (c) 2013 Zheng Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
